91bcdd2057e8ca41aaf256ce6dafb52debcc1d87
rishabhg247 <115799537+rishabhg247@users.noreply.github.com>
2023-02-12 23:50:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const port=process.env.PORT || 6010;
+const port=8080;
 const router = require('./routers/booking');
 const cors = require('cors');
 app.use(cors());
