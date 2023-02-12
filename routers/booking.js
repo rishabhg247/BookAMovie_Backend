1023fe90b8a13e8ed51dcc9450cdec0c65f3fda5
rishabhg247 <115799537+rishabhg247@users.noreply.github.com>
2023-02-12 19:50:22 +0530

    Updated response of test api

--- routers/booking.js
+++ routers/booking.js
@@ -22,5 +22,6 @@ router.post("/api/booking", async (req, res) => {
     await Booking.find().exec((err,data)=>{err?res.send(err):res.send(data[data.length-1])});
     return res.status(200);
   })
-router.get('/',(req,res)=>{res.send('hello world');})
+ //test api..
+router.get('/',(req,res)=>{res.send('Hello..This is test API for my BookAMovie project...');})
   module.exports = router;
